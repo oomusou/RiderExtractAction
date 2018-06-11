5816435142ad71e15c903ac2651e51ad3058dca5
oomusou <oomusou@gmail.com>
2018-06-11 22:51:34 +0800

    Before Refactoring

---
+++ ConsoleApp/OrderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class OrderService
+    {
+        public double CalculatePrice()
+        {
+            var price = 2000.0;
+            var discountPrice = price - 100.0;
+            
+            Console.WriteLine("Happy New Year!!");
+
+            return discountPrice;
+        }
+    }
+}
